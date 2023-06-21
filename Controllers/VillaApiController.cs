9ab3bb8ffbc18f1451b80186030d143c871528f0
Michael Olaiya <P9181@fidelitybank.ng>
2023-06-21 23:20:12 +0100

    Development Completed

---
+++ Controllers/VillaApiController.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WEBAPI1.Data;
+using WEBAPI1.Models;
+using WEBAPI1.Models.Dto;
+
+namespace WEBAPI1.Controllers
+{
+    [Route("api/VillaApi")]
+    [ApiController]
+    public class VillaApiController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaApiController(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+  
+            return Ok(_db.Villas.ToList());
+        }
+
+        [HttpGet("{id:int}", Name = "GetVilla")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = _db.Villas.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
+        {
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+
+            if (_db.Villas.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("", "Villa already Exist!");
+                return BadRequest(ModelState);
+            }
+
+            if (villaDTO == null)
+            {
+                return BadRequest(villaDTO);
+            }
+            if (villaDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            Villa model = new()
+            {
+                Amenity = villaDTO.Amenity,
+                Details = villaDTO.Details,
+                Id = villaDTO.Id,
+                ImageUrl = villaDTO.ImageUrl,
+                Name = villaDTO.Name,
+                Occupancy = villaDTO.Occupancy,
+                Rate = villaDTO.Rate,
+                Sqft = villaDTO.Sqft,
+                CreatedDate = DateTime.Now,
+                UpdatedDate = DateTime.Now
+            };
+            _db.Villas.Add(model);
+            _db.SaveChanges();
+
+            return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
+
+        }
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpDelete("{id:int}", Name = "DeleteVilla")]
+        public IActionResult DeleteVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = _db.Villas.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            _db.Villas.Remove(villa);
+            _db.SaveChanges();
+            return NoContent();
+        }
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPut("{id:int}", Name = "UpdateVilla")]
+        public IActionResult UpdateVilla(int id, [FromBody] VillaDTO villaDTO)
+        {
+            if (villaDTO == null || id != villaDTO.Id) 
+            {
+                return BadRequest();
+            }
+
+            var villa = _db.Villas.AsNoTracking().FirstOrDefault(u => u.Id == id);
+
+            Villa model = new()
+            {
+                Amenity = villaDTO.Amenity,
+                Details = villaDTO.Details,
+                Id = villaDTO.Id,
+                ImageUrl = villaDTO.ImageUrl,
+                Name = villaDTO.Name,
+                Occupancy = villaDTO.Occupancy,
+                Rate = villaDTO.Rate,
+                Sqft = villaDTO.Sqft,
+                UpdatedDate = DateTime.Now,
+                CreatedDate = villa.CreatedDate
+            };
+            _db.Villas.Update(model);
+            _db.SaveChanges();
+            return NoContent();
+        }
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPatch("{id:int}", Name = "UpdatePartialVilla")]
+        public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaDTO> patchDTO)
+        {
+            if (patchDTO == null || id == 0) { return BadRequest(); }
+
+            var villa = _db.Villas.AsNoTracking().FirstOrDefault(u => u.Id == id);
+
+            VillaDTO villaDTO = new()
+            {
+                Amenity = villa.Amenity,
+                Details = villa.Details,
+                Id = villa.Id,
+                ImageUrl = villa.ImageUrl,
+                Name = villa.Name,
+                Occupancy = villa.Occupancy,
+                Rate = villa.Rate,
+                Sqft = villa.Sqft,
+                UpdatedDate = DateTime.Now,
+                CreatedDate = villa.CreatedDate
+            };
+
+            if (villa == null) {  return BadRequest(); }
+
+            patchDTO.ApplyTo(villaDTO, ModelState);
+            Villa model = new Villa()
+            {
+                Amenity = villaDTO.Amenity,
+                Details = villaDTO.Details,
+                Id = villaDTO.Id,
+                ImageUrl = villaDTO.ImageUrl,
+                Name = villaDTO.Name,
+                Occupancy = villaDTO.Occupancy,
+                Rate = villaDTO.Rate,
+                Sqft = villaDTO.Sqft,
+                UpdatedDate = DateTime.Now,
+                CreatedDate = villa.CreatedDate
+            };
+
+            _db.Villas.Update(model);
+            _db.SaveChanges();
+            if (!ModelState.IsValid) { return BadRequest(); }
+
+            return NoContent();
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# WEBAPI1
+Certainly! Here's an example of a README summary for a RESTful API named "Villa" built with .NET Core:
+
+# Villa API
+
+The Villa API is a RESTful API built with .NET Core that allows users to perform various operations related to villas, including retrieving a list of villas, creating a new villa, updating existing villas, and retrieving a specific villa by ID.
+
+## Features
+
+- Get a list of villas
+- Create a new villa
+- Get a specific villa by ID
+- Update an existing villa
+- Patch (partially update) an existing villa
+- Delete an existing villa by ID
+
+## Technologies Used
+
+- .NET Core: [7.0]
+- Entity Framework Core: [8.0]
+
+
+
+## Getting Started
+
+Follow these steps to get the Villa API up and running on your local machine:
+
+1. Clone the repository: `git clone https://github.com/MichealO1/WEBAPI1.git`
+2. Navigate to the project directory: `cd VillaAPI`
+3. Restore the project dependencies: `dotnet restore`
+4. Configure the API settings: Update the `appsettings.json` file with your desired configurations.
+5. Set up the database: [Provide instructions for setting up and configuring the database]
+6. Apply database migrations: `dotnet ef database update`
+7. Build the solution: `dotnet build`
+8. Run the API: `dotnet run`
+9. The Villa API should now be accessible at [API endpoint URL]
+
+
+To run the tests for the Villa API, follow these steps:
+
+1. Ensure you have the necessary test dependencies installed.
+2. Set up a test database or configure the test environment.
+3. Run the tests: `dotnet test`
+
+
+## License 
+
+This project is licensed under the MIT License.
+
+## Acknowledgements
+
+This project was inspired by the need to understand and demonstrate RESTful API Using .NET Core.
+
+Special thanks to the contributors and maintainers of the frameworks and libraries used in this project.
